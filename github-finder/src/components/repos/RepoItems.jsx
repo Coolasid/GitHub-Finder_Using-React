e88f52fc0b7f0847d13d4b19238a49b9a2e0ac3a
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-02-20 21:41:26 +0530

    20-2

--- github-finder/src/components/repos/RepoItems.jsx
+++ github-finder/src/components/repos/RepoItems.jsx
@@ -12,9 +12,21 @@ function RepoItems({repo}) {
                     <FaLink className='inline mr-1'></FaLink> {name}
                 </a>
             </h3>
-            <p className='mb-3'>
-
-            </p>
+            <p className='mb-3'>{description}</p>   
+            <div>
+                <div className='mr-2 badge badge-info badge-lg'>
+                    <FaEye className='mr-2'></FaEye> {watchers_count}
+                </div>
+                <div className='mr-2 badge badge-success badge-lg'>
+                    <FaStar className='mr-2'></FaStar> {stargazers_count}
+                </div>
+                <div className='mr-2 badge badge-error badge-lg'>
+                    <FaInfo className='mr-2'></FaInfo> {open_issues }
+                </div>
+                <div className='mr-2 badge badge-warning badge-lg'>
+                    <FaUtensils className='mr-2'></FaUtensils> {forks}
+                </div>
+            </div> 
         </div>
     </div>
   )
